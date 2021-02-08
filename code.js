cd03ea7876454ec63db934e3d54d1954260ebb80
RunOrFight <dragoinfury@gmail.com>
2021-02-08 11:24:57 +0200

    test git

---
+++ code.js
@@ -0,0 +1,46 @@
+/*   ПЕЛЕМЕННЫЕ 
+var num = 5;
+const con = 6;
+console.log("Число " + num);
+console.log(con);
+
+var num_1 = 10;
+var num_2 = 3;
+var sum = num_1 - num_2;
+console.log (sum);
+console.log("Результат " + (num_1 - num_2));
+
+var num_3 = 3;
+num_3 +=1;
+num_3 ++;
+console.log(num_3);
+
+var str_1  = "23";
+var str_2 = "33";
+var result =Number(str_1) + Number( str_2);
+console.log(result);
+
+var rnd = Math.round(5.6);
+console.log(rnd);*/
+
+
+var num = true;
+if (num == true){
+    console.log("Yes");
+}
+else if (num == 3 && num == 4 || num == 6);
+else{
+    console.log("No");
+}
+var str ="4";
+switch (str){
+    case 4:
+        console.log("Number");
+        break;
+    case "4":
+        console.log("String");
+        break;
+    default:
+        console.log("Unknow");
+        break;        
+}
